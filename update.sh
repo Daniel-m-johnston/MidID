7ce19fb9d43cd83cc319aac80ab7caf27f84da79
Daniel Johnston <danielmarlowejohnston@gmail.com>
2019-06-07 11:17:53 -0400

    update script now downloads all images, skipping unavailable ones

--- update.sh
+++ update.sh
@@ -10,10 +10,16 @@ function download_year {
     for j in {0000..9999}; do
 		    download_mid $class_year$j
 	  done
+    cd ../../..
 }
 
 function download_mid {
-	 $1
+    url="https://usna.blackboard.com/bbcswebdav/orgs/DEPTCSERV/Midn%20Photos/20${1:0:2}/M$1.jpg"
+    # curl only the header and get the status code to check if valid alpha
+    response=$(curl -s -o /dev/null -I -w "%{http_code}" $url)
+    if [ $response -eq 200 ]; then
+        curl $url > m$1.jpg
+    fi
 }
 
 year=$(date '+%Y')
@@ -28,3 +34,11 @@ for i in {0..3}; do
 	download_year $((start_year + i))
 done
 
+pickle_dir=embeddings_pkl
+if [ -d $pickle_dir ]
+mv $pickle_dir $pickle_dir.$(date +%Y.%m.%d)
+mkdir $pickle_dir
+
+for x in imgs/MIDS/*; do
+    python3 cgi-bin/embeddings.py $x
+done
