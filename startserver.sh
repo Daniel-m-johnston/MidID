a0f4c8defddbca5eab6ef25eee821c3326d2115f
Daniel Johnston <danielmarlowejohnston@gmail.com>
2019-06-11 12:46:34 -0400

    fix issue with server dying early

--- startserver.sh
+++ startserver.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-echo $$
-echo $BASHPID
 
 source venv/bin/activate
-python3 -m http.server --cgi &
-firefox localhost:8000 -newwindow -private
+python3 -m http.server --cgi 2> /dev/null &
+firefox localhost:8000 -newwindow -private 2> /dev/null
 
+read -n 1 -p 'Press any key to kill server... '
+echo ''
 kill -- -$$
 
